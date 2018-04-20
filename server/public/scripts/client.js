0d14346b0f1fc2d84b7312b6764cf45d0c9de49b
Kevin Dexter <kevin.dexter@comcast.net>
2018-04-20 16:33:23 -0500

    Create button handlers

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,38 @@ $(document).ready(onReady);
 
 function onReady () {
   console.log('jQuery loaded');
+  $('.operation').on('click', runOperation);
+  $('#btnClear').on('click', clearResults);
+}
+
+function runOperation () {
+  let firstNum = $('#firstValue');
+  let secondNum = $('#secondValue');
+  let result = '<li>It worked</li>';
+  switch ($(this).text()) {
+    case '+':
+      console.log('+');
+      break;
+
+    case '-':
+      console.log('-');
+      break;
+      
+    case '/':
+      console.log('/');
+      break;
+      
+    case '*':
+      console.log('*');
+      break;
+      
+    default:
+      console.log('awww');
+      break;
+  }
+  $('#resultsDiv').append(result);
+}
+
+function clearResults () {
+  $('#resultsDiv').empty();
 }
